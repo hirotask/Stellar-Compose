30e6b4e7b653ad38331a3977bafe6ee474bd0999
hirotask <hirotaskhashi@gmail.com>
2023-01-24 21:38:41 +0900

    First commit

---
+++ src/main/kotlin/com/github/hirotask/Main.kt
@@ -0,0 +1,13 @@
+package com.github.hirotask
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+}
